3f7c9fc3e9749ac1f67e85b1b50eb607a6349003
Dev43 <patrick.guay43@gmail.com>
2017-01-24 16:49:18 -0500

    Changing  HTML and CSS for the tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+const MAXCHAR = 140;
+$('.new-tweet form textarea').on('keyup', function(event){
+    let count = $(this).val().length;
+    let counterElement = $(this).siblings(".counter");
+    counterElement.removeClass("max-characters");
+    if(count > MAXCHAR){
+      count = MAXCHAR - count;
+      counterElement.addClass("max-characters");
+    }
+    console.log(count);
+
+   counterElement[0].innerText = count;
+})
+
+});
