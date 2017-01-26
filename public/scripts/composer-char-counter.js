6cf93c3ab07628d98adb39336ae357beaa9205c4
Dev43 <patrick.guay43@gmail.com>
2017-01-26 12:16:49 -0500

    changed css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
 
 const MAXCHAR = 140;
-$('.new-tweet form textarea').on('keyup', function(event){
-    let count = $(this).val().length;
-    let counterElement = $(this).siblings(".counter");
-    counterElement.removeClass("max-characters");
-    if(count > MAXCHAR){
-      count = MAXCHAR - count;
-      counterElement.addClass("max-characters");
-    }
 
-   counterElement[0].innerText = count;
-})
+function charCount(element){
+  //console.log($(element).val())
+  let count = MAXCHAR - $(element).val().length;
+    let $counterElement = $(element).siblings(".counter");
+    $counterElement.removeClass("max-characters");
+    if(count < 0){
+      $counterElement.addClass("max-characters");
+    }
+  $counterElement.text(count);
+}
+
+
+$('.new-tweet form textarea').on('keyup', function(event){
+    charCount(this);
+});
+
+$('.new-tweet form textarea').on('cut', function(event){
+  console.log($(this))
+  charCount(this);
+});
 
 });
