303b74f64d6b282571ff224cddc3e791ee771718
Dev43 <patrick.guay43@gmail.com>
2017-01-26 14:22:26 -0500

    changed app to use mongoDB

--- mongo_example.js
+++ mongo_example.js
@@ -1,5 +1,4 @@
-"use strict"
-
+"use strict";
 
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
@@ -13,13 +12,30 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
- db.collection("tweets").find().toArray((err, results) => {
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
     if (err) throw err;
 
-    console.log("results array: ", results);
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
 
-    // This is the end...
     db.close();
   });
+
 });
